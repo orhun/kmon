57c80983e8b97a8bbe104dcb257f069eefa656f0
orhun <orhunparmaksiz@gmail.com>
2019-11-05 10:07:41 +0300

    feat: Print the version using Cargo metadata if possible

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-const VERSION:&str = "0.1.0";
+const VERSION: &'static str = "0.1.0";
+const PKG_VERSION:Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 
 fn main() {
-    println!("kmon v{}", VERSION);
+    println!("kmon v{}", PKG_VERSION.unwrap_or(VERSION));
 }

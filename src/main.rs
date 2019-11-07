16f41df8f077f12532980f07fdd3d0051c61a5d6
orhun <orhunparmaksiz@gmail.com>
2019-11-07 11:21:00 +0300

    feat: Create exec_cmd function for testing command execution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 extern crate clap;
 use clap::{Arg, App};
+use std::process::Command;
+use std::io::{self, Write};
 
 const VERSION: &'static str = "0.1.0";
+
+fn exec_cmd() {
+    let output = Command::new("ls")
+                        .arg("-a")
+                        .output()
+                        .expect("failed to execute command");
+    println!("status: {}", output.status);
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
+}
 
 fn parse_args() {
     let matches = App::new("kmon")
@@ -11,4 +23,5 @@ fn parse_args() {
 
 fn main() {
     parse_args();
+    exec_cmd();
 }

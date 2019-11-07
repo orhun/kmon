91146b47147589a1f6a47cc980c7c535061f83c8
orhun <orhunparmaksiz@gmail.com>
2019-11-07 10:36:58 +0300

    feat: Undo fetching version from Cargo configuration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 const VERSION: &'static str = "0.1.0";
-const PKG_VERSION:Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 
 fn main() {
-    println!("kmon v{}", PKG_VERSION.unwrap_or(VERSION));
+    println!("kmon v{}", VERSION);
 }

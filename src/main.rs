226ef715bad4543165e9091ebb39d3b523094e76
orhun <orhunparmaksiz@gmail.com>
2019-11-07 12:03:05 +0300

    feat: Execute system command with exec_cmd function

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ use std::io::{self, Write};
 
 const VERSION: &'static str = "0.1.0";
 
-fn exec_cmd() {
-    let output = Command::new("ls")
-                        .arg("-a")
+fn exec_cmd(cmd: &str, cmd_args: &[&str]) -> String {
+    let output = Command::new(cmd)
+                        .args(cmd_args)
                         .output()
                         .expect("failed to execute command");
-    println!("status: {}", output.status);
-    io::stdout().write_all(&output.stdout).unwrap();
     io::stderr().write_all(&output.stderr).unwrap();
+    if output.status.success() {
+        return String::from_utf8(output.stdout).expect("Not UTF-8");
+    } else {
+        return String::from("woo");
+    }
 }
 
 fn parse_args() {
@@ -23,5 +26,5 @@ fn parse_args() {
 
 fn main() {
     parse_args();
-    exec_cmd();
+    println!("{}", exec_cmd("ls", &["-a", "-x"]));
 }

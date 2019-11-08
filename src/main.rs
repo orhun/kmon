04766fc2405556795dbd80a543af5adc3d77114b
orhun <orhunparmaksiz@gmail.com>
2019-11-08 23:25:27 +0300

    style: Update indentation of variable declarations

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,8 @@ use std::io::{self, Write};
 const VERSION: &'static str = "0.1.0";
 
 fn exec_cmd(cmd: &str, cmd_args: &[&str]) -> String {
-    let output = Command::new(cmd)
-                        .args(cmd_args)
-                        .output()
-                        .expect("failed to execute command");
+    let output = Command::new(cmd).args(cmd_args)
+        .output().expect("failed to execute command");
     io::stderr().write_all(&output.stderr).unwrap();
     if output.status.success() {
         return String::from_utf8(output.stdout).expect("not UTF-8");
@@ -20,8 +18,7 @@ fn exec_cmd(cmd: &str, cmd_args: &[&str]) -> String {
 
 fn parse_args() {
     let _matches = App::new("kmon")
-                    .version(VERSION)
-                    .get_matches();
+        .version(VERSION).get_matches();
 }
 
 fn main() {

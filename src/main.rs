f275759a97b75baf2599011b89c5ffbfc71dbe86
orhun <orhunparmaksiz@gmail.com>
2019-11-11 06:55:04 +0300

    doc: Add comments to exec_cmd function

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,17 @@ use std::io::{self, Write};
 
 const VERSION: &'static str = "0.1.0";
 
+/**
+ * Execute a operating system command and return its output.
+ *
+ * @param  cmd
+ * @param  cmd_args
+ * @return output
+ */
 fn exec_cmd(cmd: &str, cmd_args: &[&str]) -> String {
     let output = Command::new(cmd).args(cmd_args)
         .output().expect("failed to execute command");
+    /* Write error output to stderr stream. */
     io::stderr().write_all(&output.stderr).unwrap();
     if output.status.success() {
         return String::from_utf8(output.stdout).expect("not UTF-8");

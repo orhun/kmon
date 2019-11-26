95ff5a2d87fc7c26e66a5d9c35c478f673a138b8
orhun <orhunparmaksiz@gmail.com>
2019-11-26 13:00:01 +0300

    refactor: Use one line if statements in up/down keys case

--- src/main.rs
+++ src/main.rs
@@ -275,15 +275,10 @@ fn create_term(args: clap::ArgMatches) -> Result<(), failure::Error> {
                 Key::Down | Key::Up => {
                     if input == Key::Down {
                         selected_index += 1;
-                        if selected_index > kernel_modules.len() - 1 {
-                            selected_index = 0;
-                        }
+                        if selected_index > kernel_modules.len() - 1 { selected_index = 0; }
                     } else {
-                        if selected_index > 0 {
-                            selected_index -= 1;
-                        } else {
-                            selected_index = kernel_modules.len() - 1;
-                        }
+                        if selected_index > 0 { selected_index -= 1; }
+                        else { selected_index = kernel_modules.len() - 1; }
                     }
                     module_info = exec_cmd("modinfo",
                         &[kernel_modules[selected_index][0].split(" (").collect::<Vec<&str>>()[0]])

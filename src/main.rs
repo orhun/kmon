3c7b61f5ce0a52803d2ec41b2eef7d5292ba1272
orhun <orhunparmaksiz@gmail.com>
2019-11-29 11:34:54 +0300

    style: Update printing module list percentage format

--- src/main.rs
+++ src/main.rs
@@ -327,7 +327,7 @@ fn create_term(args: clap::ArgMatches) -> Result<(), failure::Error> {
                             .title_style(Style::default().modifier(Modifier::BOLD))
                             .borders(Borders::ALL)
                             .title(&format!(
-                                "Loaded Kernel Modules ({}/{}) ~ %{}",
+                                "Loaded Kernel Modules ({}/{}) [{}%]",
                                 module.index + 1,
                                 kernel_modules.len(),
                                 ((module.index + 1) as f64 / kernel_modules.len() as f64 * 100.0)

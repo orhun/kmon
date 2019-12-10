6f31cc866734f9949b1a2e901264798c5b848232
orhun <orhunparmaksiz@gmail.com>
2019-12-10 16:04:33 +0300

    style: Update areas in example kernel module

--- example/lkm_example.c
+++ example/lkm_example.c
@@ -4,16 +4,16 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("orhun");
-MODULE_DESCRIPTION("A simple example Linux module.");
-MODULE_VERSION("0.01");
+MODULE_DESCRIPTION("An example Linux kernel module");
+MODULE_VERSION("0.1");
 
 static int __init lkm_example_init(void) {
-    printk(KERN_INFO "Hello World!\n");
+    printk(KERN_INFO "[+] Example kernel module loaded.\n");
     return 0;
 }
 
 static void __exit lkm_example_exit(void) {
-    printk(KERN_INFO "Goodbye World!\n");
+    printk(KERN_INFO "[-] Example kernel module unloaded.\n");
 }
 
 module_init(lkm_example_init);

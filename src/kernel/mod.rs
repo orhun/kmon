08b76c7bde9542f62b2824372d3c2b9b810b7f55
orhun <orhunparmaksiz@gmail.com>
2019-12-21 23:53:43 +0300

    style: Update the formatting of kernel mod.rs file

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -1,7 +1,8 @@
+/* Kernel module commands */
+pub mod cmd;
+/* Kernel information */
+pub mod info;
 /* Kernel activity logs */
 pub mod lkm;
 /* Loadable kernel modules */
 pub mod log;
-/* Kernel module commands */
-pub mod cmd;
-pub mod info;

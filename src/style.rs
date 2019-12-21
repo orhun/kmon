815b4ebbc84d5c6268b596cff60185e62438c7f3
orhun <orhunparmaksiz@gmail.com>
2019-12-21 22:13:10 +0300

    style: Fix the indent formatting of style module comments

--- src/style.rs
+++ src/style.rs
@@ -10,7 +10,7 @@ pub struct Style {
 }
 
 impl Default for Style {
-    /**
+	/**
 	 * Create a default style instance.
 	 *
 	 * @return Style
@@ -34,7 +34,7 @@ pub struct StyledText<'a> {
 }
 
 impl Default for StyledText<'_> {
-    /**
+	/**
 	 * Create a default styled text instance.
 	 *
 	 * @return StyledText
@@ -48,11 +48,11 @@ impl Default for StyledText<'_> {
 }
 
 impl<'a> StyledText<'a> {
-    /**
-     * Get a vector of Text widget from styled text.
-     *
-     * @return vector
-     */
+	/**
+	 * Get a vector of Text widget from styled text.
+	 *
+	 * @return vector
+	 */
 	pub fn get(&'a self) -> Vec<Text<'a>> {
 		if self.styled_text.is_empty() {
 			vec![Text::raw(&self.raw_text)]
@@ -60,38 +60,38 @@ impl<'a> StyledText<'a> {
 			self.styled_text.to_vec()
 		}
 	}
-    /**
-     * Set a raw text.
-     *
-     * @param text
-     */
+	/**
+	 * Set a raw text.
+	 *
+	 * @param text
+	 */
 	pub fn set_raw_text(&mut self, text: String) {
 		self.raw_text = text;
 		self.styled_text = Vec::new();
 	}
-    /**
-     * Set a styled text.
-     *
-     * @param text
-     * @param newline_count
-     */
+	/**
+	 * Set a styled text.
+	 *
+	 * @param text
+	 * @param newline_count
+	 */
 	pub fn set_styled_text(
 		&mut self,
 		text: Vec<Text<'static>>,
 		newline_count: usize,
 	) {
 		self.styled_text = text;
-        /* Append empty strings as much as newlines. */
+		/* Append empty strings as much as newlines. */
 		for _i in 0..newline_count * 2 {
 			self.styled_text.push(Text::raw(""));
 		}
 		self.raw_text = String::new();
 	}
-    /**
-     * Return the line count of styled text.
-     *
-     * @return usize
-     */
+	/**
+	 * Return the line count of styled text.
+	 *
+	 * @return usize
+	 */
 	pub fn lines(&self) -> usize {
 		if !self.raw_text.is_empty() {
 			self.raw_text.lines().count()

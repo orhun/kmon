037fb5f07c6829344fe227f90069d3d6763c9675
orhun <orhunparmaksiz@gmail.com>
2019-12-27 22:12:20 +0300

    fix: Remove unused crate use in main file

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod kernel;
 mod style;
 mod util;
 use app::{App, Blocks, InputMode, ScrollDirection};
-use clipboard::ClipboardProvider;
 use enum_unitary::{Bounded, EnumUnitary};
 use event::{Event, Events};
 use kernel::cmd::ModuleCommand;

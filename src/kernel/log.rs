1d9cfe670b5651b1289282a76e77c07e527baddf
orhun <orhunparmaksiz@gmail.com>
2019-12-31 09:17:39 +0300

    fix: Unwrap dmesg output result for error handling

--- src/kernel/log.rs
+++ src/kernel/log.rs
@@ -19,7 +19,7 @@ impl KernelLogs {
 	pub fn update(&mut self) -> bool {
 		self.output =
 			util::exec_cmd("dmesg", &["--kernel", "--human", "--color=never"])
-				.expect("failed to retrieve dmesg output");
+				.unwrap_or_else(|_| String::from("failed to retrieve dmesg output"));
 		let logs_updated = self.output.lines().next().unwrap() != self.last_line;
 		self.last_line = self.output.lines().next().unwrap().to_string();
 		logs_updated

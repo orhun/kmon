7c88763b115bdd814a8bb53416f80d3b6020acc4
orhun <orhunparmaksiz@gmail.com>
2020-01-06 16:11:31 +0300

    test: Add test case for info module

--- src/kernel/info.rs
+++ src/kernel/info.rs
@@ -61,3 +61,16 @@ impl KernelInfo {
 		.into_iter()
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	#[test]
+	fn test_info() {
+		let mut kernel_info = KernelInfo::new();
+		kernel_info.next();
+		assert_eq!("Kernel Version", kernel_info.current_info[0]);
+		assert_eq!(util::exec_cmd("uname", &["-v"])
+					.unwrap(), kernel_info.current_info[1]);
+	}
+}

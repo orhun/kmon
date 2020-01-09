1dee2a3ba804120a4442da0d086aaa179db5c4f8
orhun <orhunparmaksiz@gmail.com>
2020-01-09 18:38:45 +0300

    doc: Update app initialization comment in main

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ fn start_tui<B>(
 where
 	B: Backend,
 {
-	/* Configure application and styles. */
+	/* Configure the application and styles. */
 	let app_style = Style::new(args);
 	let mut app = App::new(Blocks::ModuleTable, app_style);
 	/* Draw terminal and render the widgets. */

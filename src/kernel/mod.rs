97c7a5dac4ecea6baffb3726c056e76c75758e13
orhun <orhunparmaksiz@gmail.com>
2020-01-09 20:36:21 +0300

    doc: Add comment to Kernel struct

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -7,6 +7,7 @@ use info::KernelInfo;
 use lkm::KernelModules;
 use log::KernelLogs;
 
+/* Kernel struct for logs, information and modules */
 pub struct Kernel {
 	pub logs: KernelLogs,
 	pub info: KernelInfo,

98fcbefd30d2807d3bf48e55bcfe78c4c399fe96
orhun <orhunparmaksiz@gmail.com>
2020-01-09 17:13:33 +0300

    feat: Create Kernel struct for logs, info and modules

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -2,3 +2,23 @@ pub mod cmd;
 pub mod info;
 pub mod lkm;
 pub mod log;
+use log::KernelLogs;
+use info::KernelInfo;
+use lkm::KernelModules;
+use clap::ArgMatches;
+
+pub struct Kernel {
+    pub logs: KernelLogs,
+    pub info: KernelInfo,
+    pub modules: KernelModules<'static>,
+}
+
+impl Kernel {
+    pub fn new(args: &ArgMatches) -> Self {
+        Self {
+            logs: KernelLogs::default(),
+            info: KernelInfo::new(),
+            modules: KernelModules::new(args),
+        }
+    }
+}

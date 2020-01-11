609600b89bd6e5a929fc804166fdb70b5d6f1a5d
orhun <orhunparmaksiz@gmail.com>
2020-01-11 15:23:19 +0300

    test: Update lkm module tests about scrolling

--- src/kernel/lkm.rs
+++ src/kernel/lkm.rs
@@ -357,7 +357,7 @@ mod tests {
 		let mut kernel_modules = KernelModules::new(list_args, Style::new(&args));
 		kernel_modules.args.sort = SortType::Name;
 		kernel_modules.refresh();
-		for direction in ScrollDirection::iter() {
+		for direction in ScrollDirection::iter().rev().chain(ScrollDirection::iter()) {
 			kernel_modules.scroll_list(*direction);
 			kernel_modules
 				.scroll_mod_info(*direction, *direction == ScrollDirection::Up);

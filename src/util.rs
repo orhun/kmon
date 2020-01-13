c6e4145fd634c828d2b9b231361808e814883e66
orhun <orhunparmaksiz@gmail.com>
2020-01-13 16:26:17 +0300

    feat: Use cargo environment variables for argument parsing

--- src/util.rs
+++ src/util.rs
@@ -17,10 +17,10 @@ macro_rules! map {
  * @return ArgMatches
  */
 pub fn parse_args(version: &str) -> clap::ArgMatches<'static> {
-	App::new("kmon")
-		.version(version)
-		.author("orhun <orhunparmaksiz@gmail.com>")
-		.about("<TODO>")
+	App::new(env!("CARGO_PKG_NAME"))
+		.version(env!("CARGO_PKG_VERSION"))
+		.author(env!("CARGO_PKG_AUTHORS"))
+		.about(env!("CARGO_PKG_DESCRIPTION"))
 		.arg(
 			Arg::with_name("color")
 				.short("c")

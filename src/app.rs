fd2af610d50044363c131d227add07f7f0435adc
orhun <orhunparmaksiz@gmail.com>
2020-01-17 14:32:43 +0300

    feat: Remove switching to ModuleInfo block on help action

--- src/app.rs
+++ src/app.rs
@@ -172,7 +172,6 @@ impl App<'_> {
 	}
 
 	pub fn show_help_message(&mut self, kernel_modules: &mut KernelModules) {
-		self.selected_block = Block::ModuleInfo;
 		kernel_modules.current_name = String::from("!Help");
 		kernel_modules.current_info.set_styled_text(
 			vec![

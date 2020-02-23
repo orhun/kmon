9b43f38e9800f1b5079baad9a0ff7f29f92b43de
orhun <orhunparmaksiz@gmail.com>
2020-02-23 13:14:57 +0300

    feat: Use alt-c key for clearing the ring buffer

--- src/main.rs
+++ src/main.rs
@@ -210,7 +210,7 @@ where
 						Key::Char('\\') | Key::Char('\t') | Key::BackTab => {
 							kernel.info.next();
 						}
-						Key::Ctrl('k') | Key::Ctrl('u') => {
+						Key::Ctrl('k') | Key::Ctrl('u') | Key::Alt('c') => {
 							kernel.modules.set_current_command(
 								ModuleCommand::Clear,
 								String::new(),

e3d9c2894b6eb86dae15647cc0356ae7e8a82c1e
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-10-01 14:51:47 +0200

    chore: Add build script for generating manpage and completions

--- README.md
+++ README.md
@@ -211,13 +211,7 @@ tar -xvzf kmon-*.tar.gz
 
 4. Move binary to `/usr/local/bin/` for running it from the terminal using `kmon` command.
 
-5. Man page could be viewed if [kmon.8](https://github.com/orhun/kmon/blob/master/man/kmon.8) file is copied to `/usr/local/man/man8/` directory.
-
-```
-cp kmon.8 /usr/local/man/man8/
-gzip /usr/local/man/man8/kmon.8
-man kmon
-```
+5. Man page and shell completions are generated at build time in `target` directory.
 
 #### Note
 
---
+++ build.rs
@@ -0,0 +1,50 @@
+#[path = "src/args.rs"]
+mod args;
+
+use clap::ValueEnum;
+use clap_complete::{self, Shell};
+use clap_mangen::Man;
+use std::env;
+use std::error::Error;
+use std::fs::{self, File};
+use std::io::Error as IoError;
+use std::path::{Path, PathBuf};
+
+fn build_shell_completions(out_dir: &Path) -> Result<(), IoError> {
+	fs::create_dir_all(out_dir)?;
+	let mut app = args::get_args();
+	let shells = Shell::value_variants();
+	for shell in shells {
+		clap_complete::generate_to(
+			*shell,
+			&mut app,
+			env!("CARGO_PKG_NAME"),
+			out_dir,
+		)?;
+	}
+	Ok(())
+}
+
+fn build_manpage(out_dir: &Path) -> Result<(), IoError> {
+	fs::create_dir_all(out_dir)?;
+	let app = args::get_args();
+	let file = out_dir.join(format!("{}.8", env!("CARGO_PKG_NAME")));
+	let mut file = File::create(&file)?;
+	Man::new(app).render(&mut file)?;
+	Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+	println!("cargo:rerun-if-changed=src/args.rs");
+	let out_dir = match env::var_os("OUT_DIR").map(PathBuf::from) {
+		None => return Ok(()),
+		Some(v) => v
+			.ancestors()
+			.nth(4)
+			.expect("failed to determine out dir")
+			.to_owned(),
+	};
+	build_manpage(&out_dir.join("man"))?;
+	build_shell_completions(&out_dir.join("completions"))?;
+	Ok(())
+}
